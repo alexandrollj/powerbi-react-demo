73357a99af034411ef924dfc06bd3ffec2006c82
= <alexandrollj@gmail.com>
2025-05-20 12:08:26 -0600

    refactor: adding powerbi client in global context

--- src/components/PowerBI/PowerBI.jsx
+++ src/components/PowerBI/PowerBI.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react";
-import * as powerbi from "powerbi-client";
+import "./PowerBI.css";
 
 const PowerBIEmbed = ({ embedInfo }) => {
   const reportRef = useRef(null);
@@ -7,9 +7,20 @@ const PowerBIEmbed = ({ embedInfo }) => {
   useEffect(() => {
     if (!embedInfo || !reportRef.current) return;
 
+    const powerbiService = window.powerbi; // used for .embed()
+    const powerbiSdk = window["powerbi-client"]; // used for .models.TokenType
+
+    console.log("powerbiService", powerbiService);
+    console.log("powerbi", powerbiSdk);
+
+    if (!powerbiService?.embed || !powerbiSdk?.models?.TokenType) {
+      console.error("Power BI SDK not fully loaded");
+      return;
+    }
+
     const reportConfig = {
       type: "report",
-      tokenType: powerbi.models.TokenType.Embed,
+      tokenType: powerbiSdk.models.TokenType.Embed,
       accessToken: embedInfo.embedToken,
       embedUrl: embedInfo.embedUrl,
       id: embedInfo.reportId,
@@ -20,8 +31,16 @@ const PowerBIEmbed = ({ embedInfo }) => {
         },
       },
     };
-    const report = powerbi.embed(reportRef.current, reportConfig);
-    return () => report?.destroy();
+
+    const report = powerbiService.embed(reportRef.current, reportConfig);
+
+    return () => {
+      if (report && typeof report.destroy === "function") {
+        report.destroy();
+      } else {
+        console.warn("No report instance to destroy");
+      }
+    };
   }, [embedInfo]);
 
   return <div ref={reportRef} className="reportClass" />;
