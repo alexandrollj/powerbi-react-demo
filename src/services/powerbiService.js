ce5207f61b4cd0c9f8b3ef4ea96ac2173d382a6f
= <alexandrollj@gmail.com>
2025-06-02 09:06:15 -0600

    feat: Add support Python backend
    Adding support for the new endpoint developed with Python.

--- src/services/powerbiService.js
+++ src/services/powerbiService.js
@@ -1,5 +1,6 @@
 export async function getPowerBIEmbedInfo() {
-  const response = await fetch("http://127.0.0.1:8000/embed-info");
+  // const response = await fetch("http://127.0.0.1:8000/embed-info");
+  const response = await fetch("http://localhost:5047/api/powerbi/embed-token");
   if (!response.ok) throw new Error("Failed to fetch embend info");
   return response.json();
 }
